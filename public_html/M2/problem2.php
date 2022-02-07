55bcf365dcac7413499116b654a1841b27d56615
thotranexe <thotran.exe@gmail.com>
2022-02-06 22:48:58 -0500

    completed hw

--- public_html/M2/problem2.php
+++ public_html/M2/problem2.php
@@ -11,6 +11,7 @@ function getTotal($arr) {
         $total+=$value;
     }
     //TODO do rounding stuff here
+    $total=round($total,2);
     echo "The total is " . var_export($total, true);
 }
 echo "Problem 2: Adding Floats<br>";

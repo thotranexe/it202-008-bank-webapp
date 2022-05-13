857319f3f5d6713b0e1bc322aaa80ff1cf8f77b8
thotranexe <thotran.exe@gmail.com>
2022-05-12 21:25:46 -0400

    more db issues :/

--- partials/nav.php
+++ partials/nav.php
@@ -31,7 +31,7 @@ session_start();
 <script src="<?php echo get_url('helpers.js'); ?>"></script>
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand show-balance" href="#">You Make It We Bank It</a>
+        <a class="navbar-brand" href="#">Make & Bank</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navContent" aria-controls="navContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>

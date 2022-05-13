e2e21c0e483b31607a93be7aa0df4ef3cf833dbb
thotranexe <thotran.exe@gmail.com>
2022-05-13 16:51:27 -0400

    fixing deposti flip

--- public_html/project/transaction_history.php
+++ public_html/project/transaction_history.php
@@ -1,4 +1,5 @@
-<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php require(__DIR__ . "/../../partials/nav.php");
+require(__DIR__ . "/../../partials/dashboard_nav.php");?>
 <?php
 
 if (!is_logged_in()) {

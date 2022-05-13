01911574a7d24da3c15a49ff2638ee3d0c7f2db2
thotranexe <thotran.exe@gmail.com>
2022-05-13 10:39:32 -0400

    2 trans

--- public_html/index.php
+++ public_html/index.php
@@ -1,4 +1,6 @@
 <?php echo "Welcome to Make it and We Bank it Bank";?>
 <nav>
-    <li class="nav-item"><a class="nav-link" href="<?php echo get_url('login.php'); ?>">enter</a></li>
+    <li class="nav-item">
+        <a class="nav-link" href="<?php echo get_url('login.php'); ?>">enter</a>
+    </li>
 </nav>

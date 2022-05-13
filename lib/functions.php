0e1a4e24306b86a2a7beb0e920e8e6f536d730b3
thotranexe <thotran.exe@gmail.com>
2022-05-12 20:09:59 -0400

    fixing account balance not showing

--- lib/functions.php
+++ lib/functions.php
@@ -15,6 +15,7 @@ require(__DIR__ . "/sanitizers.php");
 //TODO 3: User helpers
 require(__DIR__ . "/user_helpers.php");
 
+require(__DIR__ . "/account_helpers.php");
 
 //duplicate email/username
 require(__DIR__ . "/duplicate_user_details.php");
@@ -23,5 +24,6 @@ require(__DIR__ . "/reset_session.php");
 
 require(__DIR__ . "/get_url.php");
 
-require(__DIR__ . "/accounts_helpers.php");
+
+
 ?>

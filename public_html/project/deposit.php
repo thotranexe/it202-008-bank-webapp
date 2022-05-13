8e667d0e2227e470b23660cee83e628d582beba1
thotranexe <thotran.exe@gmail.com>
2022-05-13 10:28:22 -0400

    2 trans

--- public_html/project/deposit.php
+++ public_html/project/deposit.php
@@ -19,9 +19,8 @@ is_logged_in(true);
 <?php
 if(isset($_POST["amount"])){
     $db=getDB();
-
     $idNum = get_user_id();
-
+    //get account
     $stmt = $db->prepare("SELECT account FROM BankAccounts WHERE user_ID=:userID ORDER BY created DESC LIMIT 1");
     $stmt->execute([":userID" => $idNum]);
     $userAccount = $stmt->fetch(PDO::FETCH_ASSOC);
@@ -32,6 +31,7 @@ if(isset($_POST["amount"])){
     $wdepo=-1*$deposit;
     $world='000000000000';
     $tran_type='Deposit';
+    //transaction
     $stmt = $db->prepare("INSERT INTO Transactions (account_src, account_dest, balance_change, transaction_type, expected_total) 
     VALUES(:world,:account_recieving,:deposit,:tran_type,:deposit),(:account_recieving,:world,:wdepo,:tran_type,:wdepo)");
     $stmt->bindValue(":world",$world);
@@ -41,12 +41,16 @@ if(isset($_POST["amount"])){
     $stmt->bindValue(":deposit",$deposit);
     $stmt->bindValue(":wdepo",$wdepo);
     $stmt->execute();
-
+    //uodating balances
     $nb=get_account_balance()+$deposit;
     $stmt=$db->prepare("UPDATE BankAccounts SET balance=:nb WHERE account=:account_recieving");
     $stmt->execute([":nb" => $nb,":account_recieving" => $account_recieving]);
 
-    $wnb=get_account_balance()-$deposit;
+    $stmt=$db->prepare("SELECT balance FROM BankAccounts WHERE account='000000000000'");
+    $stmt->execute();
+    $wcbal=$stmt->fetch(PDO::FETCH_ASSOC);
+    $wcbal=implode("",$wcbal);
+    $wnb=$wcbal-$deposit;
     $stmt=$db->prepare("SELECT account FROM BankAccounts WHERE account='000000000000'");
     $stmt->execute();
     $wcc=$stmt->fetch(PDO::FETCH_ASSOC);

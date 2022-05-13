8d3d23a0dcddfceb3f6303769aaa2c33f16c3d02
thotranexe <thotran.exe@gmail.com>
2022-05-13 18:09:56 -0400

    tran hist working?

--- public_html/project/transaction_history.php
+++ public_html/project/transaction_history.php
@@ -24,7 +24,7 @@ if(isset($account_id)) {
     $r = $stmt->execute(["account_id" => $userAccount]);
     if ($r) {
         $transactions = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        print_r($transactions);
+        //print_r($transactions);
     } else {
         $e = $stmt->errorInfo();
         flash("There was an error fetching transaction info " . var_export($e, true));

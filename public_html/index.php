14cbfd9cc405d7dfa35c105a6195c223ac80ba5f
thotranexe <thotran.exe@gmail.com>
2022-05-13 01:35:08 -0400

    deposit nav

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,2 @@
-<?php
-// index.php
-header("Location:$BASE_PATH/index.php");
-exit();
-?>
+<?php echo "Welcome to Make it and We Bank it Bank";?>
+<li class="nav-item"><a class="nav-link" href="<?php echo get_url('login.php'); ?>">enter</a></li>

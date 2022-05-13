a3182422dd16a0eaf8f1db28d2500520c5e578d6
thotranexe <thotran.exe@gmail.com>
2022-05-12 21:48:20 -0400

    wont show bal

--- public_html/project/login.php
+++ public_html/project/login.php
@@ -106,4 +106,4 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
 }
 ?>
 <?php
-require(__DIR__ . "/../../partials/flash.php");
+require(__DIR__ . "/../../partials/footer.php");
--- public_html/project/register.php
+++ public_html/project/register.php
@@ -91,5 +91,5 @@ if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm
 }
 ?>
 <?php
-require(__DIR__ . "/../../partials/footer.php");
+require(__DIR__ . "/../../partials/flash.php");
 ?>

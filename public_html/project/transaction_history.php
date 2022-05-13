678b5bf0a45e0114959d9486bc406a6ca1d4a440
thotranexe <thotran.exe@gmail.com>
2022-05-13 19:10:52 -0400

    transaction tweaks +transfer

--- public_html/project/transaction_history.php
+++ public_html/project/transaction_history.php
@@ -7,17 +7,24 @@ if (!is_logged_in()) {
     die(header("Location: login.php"));
 }
 
-$account_id = get_account_number();
-print($account_id);
-$db = getDB();
+$account_id = get_user_id();
+//print($account_id);
+
+$db=getDB();
+$idNum = get_user_id();
+//get accountg
+$stmt = $db->prepare("SELECT account FROM BankAccounts WHERE user_ID=:userID ORDER BY created DESC LIMIT 1");
+$stmt->execute([":userID" => $idNum]);
+$userAccount = $stmt->fetch(PDO::FETCH_ASSOC);
+$userAccount = implode("",$userAccount);
 
 if(isset($account_id)) {
 
     $stmt = $db->prepare("SELECT balance_change, transaction_type, created FROM Transactions WHERE account_src = :account_id LIMIT 10");
-    $r = $stmt->execute(["account_id" => $account_id]);
+    $r = $stmt->execute(["account_id" => $userAccount]);
     if ($r) {
         $transactions = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        print_r($transactions);
+        //print_r($transactions);
     } else {
         $e = $stmt->errorInfo();
         flash("There was an error fetching transaction info " . var_export($e, true));
@@ -26,32 +33,30 @@ if(isset($account_id)) {
 ?>
     <h3>Transaction History</h3>
     <div class="results">
-        <?php // if ($transactions): ?>
+        <?php if ($transactions): ?>
             <div class="list-group">
                 <?php foreach ($transactions as $t): ?>
                     <div class="list-group-item">
                         <div>
-                            <div>Amount:</div>
-                            <div><?php safer_echo($t["amount"]); ?></div>
+                            <span>Amount:</span>
+                            <span><?php safer_echo($t["balance_change"]); ?></span>
                         </div>
                         <div>
-                            <div>Action Type:</div>
-                            <div><?php safer_echo($t["action_type"]); ?></div>
+                            <span>Transaction Type:</span>
+                            <span><?php safer_echo($t["transaction_type"]); ?></span>
                         </div>
                         <div>
-                            <div>Memo:</div>
-                            <div><?php safer_echo($t["memo"]); ?></div>
                         </div>
                         <div>
-                            <div>Created:</div>
-                            <div><?php safer_echo($t["created"]); ?></div>
+                            <span>Created:</span>
+                            <span><?php safer_echo($t["created"]); ?></span>
                         </div>
                     </div>
                 <?php endforeach; ?>
             </div>
-        <?php //else: ?>
+        <?php else: ?>
             <p>No results</p>
-        <?php //endif; ?>
+        <?php endif; ?>
     </div>
 <?php
 require(__DIR__ . "/../../partials/footer.php");
